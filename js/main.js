461a889e13faaf0cfffc1ed7f689597a50a03112
Erik Greyan <programming011@gmail.com>
2020-06-23 12:30:39 +0400

    upload files

---
+++ js/main.js
@@ -0,0 +1,4 @@
+	function myFunc() {
+   	var myTag = document.getElementById("Բարի");
+   	alert("Բարի Գալուստ վեբ կայք GreyanStyle.am այս կայք ստեղծված է որպես տնային աշխատանք և տեսնելով աշակերտի գիտելիքները ստեղծված է HayrapetyanStyle.am վեբ կայքի պես։ Շնորակալություն");
+   }
